55b23ec3a1d0ae27c93f920974158bb2e30db47a
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-10-07 13:11:40 +0000

    introduce own shell result to easier handle stdout and stderr

--- src/fhir_scripts/tools/shell.py
+++ src/fhir_scripts/tools/shell.py
@@ -6,25 +6,58 @@ from tqdm import tqdm
 CalledProcessError = CalledProcessError
 
 
+class ShellResult:
+    def __init__(self, process):
+        self.stdout = _convert_std(process.stdout)
+        self.stderr = _convert_std(process.stderr)
+        self.returncode = process.returncode
+        self.args = process.args
+
+    @property
+    def stdout_oneline(self) -> str:
+        return _oneline(self.stdout)
+
+    @property
+    def stderr_oneline(self) -> str:
+        return _oneline(self.stderr)
+
+
+def _convert_std(input) -> list[str]:
+    if isinstance(input, bytes):
+        input = input.decode("utf-8")
+
+    return [line.strip() for line in input.strip().split("\n") if line]
+
+
+def _oneline(list_: list[str]) -> str:
+    return ", ".join(list_)
+
+
 def run(cmd, check: bool | None = None, capture_output: bool = False):
     """
     Execute a command on the shell
 
-    By default the output is not captured (`capture_output = False`), but if the status code is not equal to 0 an `CalledProcessError` is raised (`check = True`).
+    By default the output is not captured (`capture_output = False`), but if the status code is not equal to 0 an
+    `CalledProcessError` is raised (`check = True`). If `capture_output` is set to `True`, `check` is automatically set
+    to `False` except if stated otherwise.
     """
-    return subprocess.run(
+    res = subprocess.run(
         cmd,
         shell=True,
         check=check or not capture_output,
         capture_output=capture_output,
     )
 
+    return ShellResult(res)
+
 
 def run_progress(cmd, total, prefixes, desc):
     """
     Execute a command on shell with a progress bar
 
-    The output is hidden but instead a progress bar shown. It also shows a progress as X out of `total`. `prefixes` defines a list of prefixes of lines to be counted as progress and `desc` is a string that is added as a title in front of the progress bar.
+    The output is hidden but instead a progress bar shown. It also shows a progress as X out of `total`. `prefixes`
+    defines a list of prefixes of lines to be counted as progress and `desc` is a string that is added as a title in
+    front of the progress bar.
     """
     with subprocess.Popen(
         cmd,
@@ -51,6 +84,7 @@ def run_progress(cmd, total, prefixes, desc):
                 bar.refresh()
 
             if proc.returncode != 0:
-                stdout = ", ".join(list(proc.stdout)).replace("\n", "")
-                stderr = ", ".join(list(proc.stderr)).replace("\n", "")
-                raise CalledProcessError(proc.returncode, proc.args, stdout, stderr)
+                res = ShellResult(proc)
+                raise CalledProcessError(
+                    proc.returncode, proc.args, res.stdout_oneline, res.stderr_oneline
+                )
