f8bb7e47c326386652582ff8520f4b83ed0d4ba1
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-10-08 13:05:31 +0000

    extracted the log symbols for reuse

--- src/fhir_scripts/log.py
+++ src/fhir_scripts/log.py
@@ -1,14 +1,23 @@
+ERR = "❌"
+CHECK = "✅"
+INFO = "ℹ️"
+
+# These need an additional space after the symbol as they omit one
+WARN = "⚠️ "
+ARR = "➡️ "
+
+
 def fail(string: str):
-    print("❌ " + string)
+    print(f"{ERR} {string}")
 
 
 def warn(string: str):
-    print("⚠️  " + string)
+    print(f"{WARN} {string}")
 
 
 def info(string: str):
-    print("➡️  " + string)
+    print(f"{ARR} {string}")
 
 
 def succ(string: str):
-    print("✅ " + string)
+    print(f"{CHECK} {string}")
