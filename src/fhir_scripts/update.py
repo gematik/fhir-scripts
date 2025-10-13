2fb9ed7703dd15831b0010f869a461cb2ade1a45
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-10-13 12:02:46 +0000

    add update for all of the international FHIR tooling

--- src/fhir_scripts/update.py
+++ src/fhir_scripts/update.py
@@ -34,10 +34,20 @@ def update_igpub(*args, **kwargs):
     log.succ(f"Updated IG Publisher: {str(prev_version)} → {igpub.version()}")
 
 
+def update_tools(*args, **kwargs):
+    update_sushi(args, kwargs)
+    update_igpub(args, kwargs)
+
+
 def update_script(*args, **kwargs):
     pass
 
 
 __doc__ = "Update tools"
-__handlers__ = {SCRIPT: update_script, SUSHI: update_sushi, IGPUB: update_igpub}
+__handlers__ = {
+    SCRIPT: update_script,
+    SUSHI: update_sushi,
+    IGPUB: update_igpub,
+    TOOLS: update_tools,
+}
 __setup_subparser__ = setup_subparser
