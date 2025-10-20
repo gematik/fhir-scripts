0c8bb034f4f69a76e1a02efc77e2a4721ae8e257
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-10-20 10:10:02 +0000

    make config model more strict by not allowing additonal fields

--- src/fhir_scripts/models/config.py
+++ src/fhir_scripts/models/config.py
@@ -1,40 +1,44 @@
 from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
-class BuildStepDefsConfig(BaseModel):
+class StrictBaseModel(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+
+class BuildStepDefsConfig(StrictBaseModel):
     requirements: bool = False
     cap_statements: bool = False
 
 
-class BuildStepIgConfig(BaseModel):
+class BuildStepIgConfig(StrictBaseModel):
     openapi: bool = False
 
 
-class BuildStepConfig(BaseModel):
+class BuildStepConfig(StrictBaseModel):
     definitions: BuildStepDefsConfig = BuildStepDefsConfig()
     ig: BuildStepIgConfig = BuildStepIgConfig()
 
 
-class BuildConfig(BaseModel):
+class BuildConfig(StrictBaseModel):
     steps: BuildStepConfig = BuildStepConfig()
 
 
-class DeployConfig(BaseModel):
+class DeployConfig(StrictBaseModel):
     env: dict[str, str]
     path: str | None = None
 
 
-class EpaToolsArchiveConfig(BaseModel):
+class EpaToolsArchiveConfig(StrictBaseModel):
     content_files: list[Path]
 
 
-class EpaToolsConfig(BaseModel):
+class EpaToolsConfig(StrictBaseModel):
     archive: EpaToolsArchiveConfig
 
 
-class Config(BaseModel):
+class Config(StrictBaseModel):
     deploy: DeployConfig | None = None
     epatools: EpaToolsConfig | None = None
     build: BuildConfig = BuildConfig()
