9c41a79d3c1853a7c8277792e3a680c2dfa750f0
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-10-22 14:21:40 +0000

    get OpenAPI files for archive from epatools config

--- src/fhir_scripts/models/build_config.py
+++ src/fhir_scripts/models/build_config.py
@@ -1,12 +1,9 @@
-from pathlib import Path
-
 from .strict_base_model import StrictBaseModel
 
 
 class BuildBuiltinEpaToolsConfig(StrictBaseModel):
     cap_statements: bool = False
     openapi: bool = False
-    archive_files: list[Path] = []
 
 
 class BuildBuiltinConfig(StrictBaseModel):
