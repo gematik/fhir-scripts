d00f87c345b28c4be0964a68699b74b552b3e7ba
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-11-13 08:15:04 +0000

    test for confirm

---
+++ test/test_helper.py
@@ -0,0 +1,72 @@
+import unittest
+from unittest.mock import patch
+
+from fhir_scripts.exception import CancelException
+from fhir_scripts.helper import confirm
+
+
+class TestHelper(unittest.TestCase):
+
+    def test_confirm_yes(self):
+        try:
+            confirm("test", "test failed", confirm_yes=True)
+
+        except CancelException:
+            self.fail("Should not raise exception when forced")
+
+    @patch("builtins.input", return_value="y")
+    def test_confirm_input_y(self, mock_input):
+        try:
+            confirm("test", "test failed")
+
+        except CancelException:
+            self.fail("Should not raise exception when confirmed with 'y'")
+
+    @patch("builtins.input", return_value="yes")
+    def test_confirm_input_yes(self, mock_input):
+        try:
+            confirm("test", "test failed")
+
+        except CancelException:
+            self.fail("Should not raise exception when confirmed with 'yes'")
+
+    @patch("builtins.input", return_value="n")
+    def test_confirm_input_n(self, mock_input):
+        try:
+            confirm("test", "test failed")
+
+        except CancelException:
+            pass
+
+        else:
+            self.fail("Did not raise CancelException when input 'n'")
+
+    @patch("builtins.input", return_value="no")
+    def test_confirm_input_no(self, mock_input):
+        try:
+            confirm("test", "test failed")
+
+        except CancelException:
+            pass
+
+        else:
+            self.fail("Did not raise CancelException when input 'no'")
+
+    @patch("builtins.input", return_value="")
+    def test_confirm_default_yes(self, mock_input):
+        try:
+            confirm("test", "test failed", default=True)
+
+        except CancelException:
+            self.fail("Should not raise exception with default 'yes'")
+
+    @patch("builtins.input", return_value="")
+    def test_confirm_default_no(self, mock_input):
+        try:
+            confirm("test", "test failed")
+
+        except CancelException:
+            pass
+
+        else:
+            self.fail("Did not raise CancelException with default 'no'")
