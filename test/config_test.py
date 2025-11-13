cdf8ea73bf38274d41de67809339bc45c7440855
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2025-11-13 07:45:40 +0000

    test not existing config

--- test/config_test.py
+++ test/config_test.py
@@ -2,6 +2,7 @@ import unittest
 from pathlib import Path
 
 from fhir_scripts import config
+from fhir_scripts.models.config import Config
 
 
 class TestConfig(unittest.TestCase):
@@ -14,3 +15,14 @@ class TestConfig(unittest.TestCase):
 
         self.assertIsNotNone(cfg)
         self.assertIsInstance(cfg, config.Config)
+
+    def test_load_config_not_exists(self):
+        config_path = Path("examples/foo.yaml")
+        self.assertFalse(config_path.exists())
+
+        cfg = config.load(config_path)
+
+        self.assertIsNotNone(cfg)
+        self.assertIsInstance(cfg, config.Config)
+
+        self.assertEqual(cfg, Config())
