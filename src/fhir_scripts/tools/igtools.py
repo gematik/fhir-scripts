3be9fd557d133b8a0f1103f036f54cb36027fa4b
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2026-01-06 13:59:16 +0000

    remove module version of third-party integration

--- src/fhir_scripts/tools/igtools.py
+++ src/fhir_scripts/tools/igtools.py
@@ -6,14 +6,13 @@ from pathlib import Path
 
 from .. import log
 from ..exception import NoConfigException
-from .basic import python
+from ..helper import require_installed
+from .basic import python, shell
 
-# Check if igtools package is installed
-# try:
-#     importlib.metadata.version("igtools")
-#     IGTOOLS_PACKAGE_AVAILABLE = True
-# except importlib.metadata.PackageNotFoundError:
-IGTOOLS_PACKAGE_AVAILABLE = False
+VERSION_REGEX = re.compile(r"IGTOOLS\s\(v(\d+(?:\.\d+){,2})\b", re.IGNORECASE)
+PACKAGE = "git+https://github.com/onyg/req-tooling.git"
+
+config = Path("./.igtools/config.yaml")
 
 
 def is_configured(func):
@@ -31,158 +30,62 @@ def is_configured(func):
     return wrapper
 
 
-###
-# Use the module
-###
-if IGTOOLS_PACKAGE_AVAILABLE:
-    # Disable for now as the module does not provide a stable interface
-    pass
-
-    # from igtools.config import CONFIG_DEFAULT_DIR, CONFIG_FILE, Config
-    # from igtools.specifications import (
-    #     Processor,
-    #     ReleaseNoteManager,
-    #     RequirementExporter,
-    # )
-
-    # DEFAULT_EXPORT_FORMAT = "JSON"
-    # DEFAULT_EXPORT_VERSION = "current"
-
-    # config = Path(CONFIG_DEFAULT_DIR) / CONFIG_FILE
-
-    # @is_configured
-    # def process():
-    #     """
-    #     Process requirements
-    #     """
-    #     log.info("Processing requirements")
-
-    #     try:
-    #         config = Config()
-    #         config.load()
-    #         processor = Processor(config=config)
-    #         processor.process()
-
-    #     except Exception as e:
-    #         raise Exception("Failed to process requirements: " + str(e))
-
-    #     log.succ("Requirements processed")
-
-    # @is_configured
-    # def release_notes(output_dir: Path | str):
-    #     """
-    #     Update release notes
-    #     """
-    #     log.info("Updating release-notes")
-
-    #     try:
-    #         config = Config()
-    #         config.load()
-
-    #         release_note_manager = ReleaseNoteManager(config=config)
-    #         release_note_manager.generate(output=output_dir)
-
-    #     except Exception as e:
-    #         raise Exception("Failed to update release-notes: " + str(e))
-
-    #     log.succ("Release-notes updated")
-
-    # @is_configured
-    # def export(output_dir: Path | str):
-    #     """
-    #     Exports requirements
-    #     """
-    #     log.info("Export requirements")
-
-    #     try:
-    #         config = Config()
-    #         config.load()
-    #         filename = RequirementExporter.generate_filename(
-    #             format=DEFAULT_EXPORT_FORMAT, version=DEFAULT_EXPORT_VERSION
-    #         )
-    #         exporter = RequirementExporter(
-    #             config=config,
-    #             format=DEFAULT_EXPORT_FORMAT,
-    #             filename=filename,
-    #             version=DEFAULT_EXPORT_VERSION,
-    #         )
-    #         exporter.export(output=output_dir)
-
-    #     except Exception as e:
-    #         raise Exception("Failed to export requirements: " + str(e))
-
-    #     log.succ("Requirements exported successfully")
-
-    # def version(short: bool = False, *args, **kwargs) -> str | None:
-    #     """
-    #     Get the installed version
-    #     """
-    #     return importlib.metadata.version("igtools")
+@is_configured
+@require_installed("igtools", __tool_name__)
+def process():
+    """
+    Process requirements
+    """
+    log.info("Processing requirements")
+    shell.run("igtools process")
+    log.succ("Requirements processed")
 
 
-###
-# Use the command line
-###
-else:
-    from ..helper import require_installed
-    from .basic import shell
+@is_configured
+@require_installed("igtools", __tool_name__)
+def release_notes(output_dir: Path | str):
+    """
+    Update release notes
+    """
+    log.info("Updating release-notes")
+    shell.run(f"igtools ig-release-notes {str(output_dir)}")
+    log.succ("Release-notes updated")
 
-    VERSION_REGEX = re.compile(r"IGTOOLS\s\(v(\d+(?:\.\d+){,2})\b", re.IGNORECASE)
-    PACKAGE = "git+https://github.com/onyg/req-tooling.git"
 
-    config = Path("./.igtools/config.yaml")
+@is_configured
+@require_installed("igtools", __tool_name__)
+def export(output_dir: Path | str):
+    """
+    Exports requirements
+    """
+    log.info("Export requirements")
+    shell.run(f"igtools export {str(output_dir)}")
+    log.succ("Requirements exported successfully")
 
-    @is_configured
-    @require_installed("igtools", __tool_name__)
-    def process():
-        """
-        Process requirements
-        """
-        log.info("Processing requirements")
-        shell.run("igtools process", capture_output=True)
-        log.succ("Requirements processed")
 
-    @is_configured
-    @require_installed("igtools", __tool_name__)
-    def release_notes(output_dir: Path | str):
-        """
-        Update release notes
-        """
-        log.info("Updating release-notes")
-        shell.run(f"igtools ig-release-notes {str(output_dir)}", capture_output=True)
-        log.succ("Release-notes updated")
+def update(*args, **kwargs):
+    python.install(PACKAGE, as_global=True)
 
-    @is_configured
-    @require_installed("igtools", __tool_name__)
-    def export(output_dir: Path | str):
-        """
-        Exports requirements
-        """
-        log.info("Export requirements")
-        shell.run(f"igtools export {str(output_dir)}", capture_output=True)
-        log.succ("Requirements exported successfully")
 
-    def update(*args, **kwargs):
-        python.install(PACKAGE, as_global=True)
+def version(short: bool = False, *args, **kwargs) -> str | None:
+    """
+    Get the installed version of igtools, returns None if not installed
+    """
+    try:
+        res = shell.run("igtools -v", check=True, log_output=False)
 
-    def version(short: bool = False, *args, **kwargs) -> str | None:
-        """
-        Get the installed version of igtools, returns None if not installed
-        """
-        try:
-            res = shell.run("igtools -v", check=True, capture_output=True)
+        # Extract the version string from output
+        match = VERSION_REGEX.match(res.stdout_oneline)
 
-            # Extract the version string from output
-            match = VERSION_REGEX.match(res.stdout_oneline)
+        if short:
+            return match[1] if match else None
 
-            if short:
-                return match[1] if match else None
+        else:
+            return f"{match[1]} ({python.version()})" if match else None
 
-            else:
-                return f"{match[1]} ({python.version()})" if match else None
+    except shell.CalledProcessError:
+        return None
 
-        except shell.CalledProcessError:
-            return None
 
-    def latest_version(*args, **kwargs) -> str | None:
-        return python.latest_version_number(PACKAGE)
+def latest_version(*args, **kwargs) -> str | None:
+    return python.latest_version_number(PACKAGE)
--- src/fhir_scripts/tools/publishtools.py
+++ src/fhir_scripts/tools/publishtools.py
@@ -1,102 +1,59 @@
 __tool_name__ = "publishtools"
 
-import importlib.metadata
 import re
 from pathlib import Path
 
 from .. import log
-from .basic import python
+from ..helper import require_installed
+from .basic import python, shell
 
-# Check if igtools package is installed
-try:
-    importlib.metadata.version("publish_tools")
-    PUBLISHTOOLS_PACKAGE_AVAILABLE = True
-except importlib.metadata.PackageNotFoundError:
-    PUBLISHTOOLS_PACKAGE_AVAILABLE = False
+VERSION_REGEX = re.compile(r"IGTOOLS\s\(v(\d+(?:\.\d+){,2})\b", re.IGNORECASE)
+PACKAGE = "git+https://github.com/gematik/publish-tools.git"
 
 
-###
-# Use the module
-###
-if PUBLISHTOOLS_PACKAGE_AVAILABLE:
-    from publish_tools import ig, render
-
-    def publish(project_dir: Path, ig_registry: Path):
-        """
-        Publish project
-        """
-        log.info("Publish project")
-        ig.publish(project_dir, ig_registry)
-        log.succ("Project published successfully")
-
-    def render_list(ig_registry: Path):
-        """
-        Render the IG overview list
-        """
-        log.info("Render IG overview list")
-        render.render_ig_list(ig_registry)
-        log.succ("IG overview rendered successfully")
-
-    def version(short: bool = False, *args, **kwargs) -> str | None:
-        """
-        Get the installed version
-        """
-        return importlib.metadata.version("publish_tools")
+@require_installed("publishtools", __tool_name__)
+def publish(project_dir: Path, ig_registry: Path):
+    """
+    Publish project
+    """
+    log.info("Publish project")
+    shell.run(
+        f"publishtools publish --project-dir {str(project_dir)} --ig-registry {str(ig_registry)}",
+    )
+    log.succ("Project published successfully")
 
 
-###
-# Use the command line
-###
-else:
-    from ..helper import require_installed
-    from .basic import shell
+@require_installed("publishtools", __tool_name__)
+def render_list(ig_registry: Path):
+    """
+    Render the IG overview list
+    """
+    log.info("Render IG overview list")
+    shell.run(f"publishtools render-list --ig-registry {str(ig_registry)}")
+    log.succ("IG overview rendered successfully")
 
-    VERSION_REGEX = re.compile(r"IGTOOLS\s\(v(\d+(?:\.\d+){,2})\b", re.IGNORECASE)
-    PACKAGE = "git+https://github.com/gematik/publish-tools.git"
 
-    @require_installed("publishtools", __tool_name__)
-    def publish(project_dir: Path, ig_registry: Path):
-        """
-        Publish project
-        """
-        log.info("Publish project")
-        shell.run(
-            f"publishtools publish --project-dir {str(project_dir)} --ig-registry {str(ig_registry)}",
-            capture_output=True,
-        )
-        log.succ("Project published successfully")
+def update(*args, **kwargs):
+    python.install(PACKAGE, as_global=True)
 
-    @require_installed("publishtools", __tool_name__)
-    def render_list(ig_registry: Path):
-        """
-        Render the IG overview list
-        """
-        log.info("Render IG overview list")
-        shell.run(
-            f"publishtools render-list --ig-registry {str(ig_registry)}",
-            capture_output=True,
-        )
-        log.succ("IG overview rendered successfully")
 
-    def update(*args, **kwargs):
-        python.install(PACKAGE, as_global=True)
+def version(short: bool = False, *args, **kwargs) -> str | None:
+    """
+    Get the installed version of igtools, returns None if not installed
+    """
+    try:
+        res = shell.run("publishtools version", check=True, log_output=False)
+        version = res.stdout_oneline
 
-    def version(short: bool = False, *args, **kwargs) -> str | None:
-        """
-        Get the installed version of igtools, returns None if not installed
-        """
-        try:
-            res = shell.run("publishtools version", check=True, capture_output=True)
-            version = res.stdout_oneline
+        if short:
+            return version if version else None
 
-            if short:
-                return version if version else None
+        else:
+            return f"{version} ({python.version()})" if version else None
 
-            else:
-                return f"{version} ({python.version()})" if version else None
+    except shell.CalledProcessError:
+        return None
 
-        except shell.CalledProcessError:
-            return None
 
-    def latest_version(*args, **kwargs) -> str | None:
-        return python.latest_version_number(PACKAGE)
+def latest_version(*args, **kwargs) -> str | None:
+    return python.latest_version_number(PACKAGE)
